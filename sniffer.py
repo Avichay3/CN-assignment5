d8a070494c8b40095c1e9a686e455fa22ea87f16
Avichay3 <aviaimzina312@gmail.com>
2023-06-01 01:26:37 +0300

    update 2

--- sniffer.py
+++ sniffer.py
@@ -37,6 +37,10 @@ def process_udp_packet(packet):
         file.write(f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, source_port: {source_port}, dest_port: {destination_port}, protocol: UDP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
 
 def process_icmp_packet(packet):
+    """
+    This function as her name says, process ICMP packets.
+    Overall, this function do as the function above but for ICMP protocol
+    """
     source_ip = packet[IP].src
     destination_ip = packet[IP].dst
     timestamp = packet.time
@@ -46,25 +50,39 @@ def process_icmp_packet(packet):
         file.write(f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, protocol: ICMP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
 
 def process_igmp_packet(packet):
-    # IGMP packet processing logic goes here
-    pass
+    """
+    This function as her name says, process IGMP packets.
+    Overall, this function do as the function above but for IGMP protocol
+    """
+    source_ip = packet[IP].src
+    destination_ip = packet[IP].dst
+    timestamp = packet.time
+    total_length = len(packet)
+
+    with open("output.txt", "a") as file:
+        file.write(
+            f"{{ source_ip: {source_ip}, dest_ip: {destination_ip}, protocol: IGMP, timestamp: {timestamp}, total_length: {total_length}, data: {packet.payload.hex()} }}\n")
 
 def process_raw_packet(packet):
-    # RAW packet processing logic goes here
-    pass
+    """
+    handling packets of a protocol other than TCP, UDP, ICMP, or IGMP.
+    """
+
 
 def process_packet(packet):
-    if IP in packet:
-        if TCP in packet:
-            process_tcp_packet(packet)
-        elif UDP in packet:
-            process_udp_packet(packet)
-        elif ICMP in packet:
-            process_icmp_packet(packet)
-        elif IGMP in packet:
-            process_igmp_packet(packet)
-        else:
-            process_raw_packet(packet)
+    """
+    check the protocol type.
+    """
+    if TCP in packet:
+        process_tcp_packet(packet)
+    elif UDP in packet:
+        process_udp_packet(packet)
+    elif ICMP in packet:
+        process_icmp_packet(packet)
+    elif IGMP in packet:
+        process_igmp_packet(packet)
+    else:
+        process_raw_packet(packet)
 
-# Capture packets and process them
+
 sniff(filter="", prn=process_packet)
